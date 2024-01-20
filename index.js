6bded6776f71b8f9d013a44bce072086e1b92ecb
Dennis <ngetichdennis90@gmail.com>
2024-01-20 22:09:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
 /* Your Code Here */
+// Function 1: createEmployeeRecord
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+// Function 2: createEmployeeRecords
+function createEmployeeRecords(arrOfArrays) {
+    return arrOfArrays.map(createEmployeeRecord);
+}
+
+// Function 3: createTimeInEvent
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    });
+    return this;
+}
+
+// Function 4: createTimeOutEvent
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    });
+    return this;
+}
+
+// Function 5: hoursWorkedOnDate
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100; // Convert to hours
+}
+
+// Function 6: wagesEarnedOnDate
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+}
+
+// Function 7: allWagesFor (provided in the challenge)
+// No need to implement as it is provided.
+
+// Function 8: findEmployeeByFirstName
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+// Function 9: calculatePayroll
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPay, employee) => totalPay + allWagesFor.call(employee), 0);
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
